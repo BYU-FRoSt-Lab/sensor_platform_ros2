50c8f7da349f5b2c698977ba2d4bfaa8c6c032bb
Braden <bjz-github@spwiz.com>
2025-12-19 15:01:47 -0700

    Update to the launch and ntrip client launch

--- packages/sensor_bringup/launch/sensor_platform_launch.py
+++ packages/sensor_bringup/launch/sensor_platform_launch.py
@@ -1,6 +1,8 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
@@ -21,24 +23,28 @@ def generate_launch_description():
     )
 
 	return LaunchDescription([
+		DeclareLaunchArgument('namespace', default_value='/'),
 		Node(
 			package='sbg_driver',
 		#	name='sbg_device_1',
 			executable = 'sbg_device',
 			output = 'screen',
+			namespace=LaunchConfiguration('namespace'),
 			parameters = [sbg_config]
 		),
 		Node(
 			package='nmea_gpsd',
 			executable='nmea_gpsd_udp',
 			output='screen',
+			namespace=LaunchConfiguration('namespace'),
 			parameters=[nmea_config]
         ),
 		Node(
-                name='ntrip_client',
-                package='ntrip_client',
-                executable='ntrip_ros.py',
-				parameters=[ntrip_config]
-        ),
+            package='ntrip_client',
+            executable='ntrip_ros.py',
+            name='ntrip_client',
+            namespace=LaunchConfiguration('namespace'),
+            parameters=[ntrip_config],
+        )
 		
 	])
